d2dd4971b9ceacea0a039b76709691d080cc65cc
test <aponomarev@seizara.com>
2019-02-19 17:35:38 +0300

    start

---
+++ src/js/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+    const cartWrapper = document.querySelector('.cart__wrapper'), 
+    cart = document.querySelector('.cart'),
+    close = document.querySelector('.cart__close'),
+    open = document.querySelector('#cart'),
+    goodsBtn = document.querySelectorAll('.goods__btn'),
+    products = document.querySelectorAll('.goods__item'),
+    confirm = document.querySelector('.confirm'),
+    badge = document.querySelector('.nav__badge'),
+    totalCost = document.querySelector('.cart_total > span'),
+    titles = document.querySelectorAll('goods__title');
+
+    console.log(goodsBtn);
